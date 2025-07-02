10e4ceaadedd760f803eaad28ebbd5855aea345f
Marios <mrst5gs@yahoo.gr>
2025-07-02 17:00:02 +0300

    Create class that takes employee or manager as parameter and prints name

---
+++ BlazorApp/Data/PersonelNamePrinter.cs
@@ -0,0 +1,35 @@
+﻿namespace BlazorApp.Data
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+    }
+
+    public class Manager
+    {
+        public string Name { get; set; }
+    }
+
+    public class PersonelNamePrinter
+    {
+        public void Print(Employee employee)
+        {
+            if (employee != null)
+            {
+                return;
+            }
+
+            Console.WriteLine(employee.Name);
+        }
+
+        public void Print(Manager manager)
+        {
+            if(manager != null)
+            {
+                return;
+            }
+
+            Console.Write(manager.Name);
+        }
+    }
+}
